21d139b45da644a4da30bc13929b1ebafc8fabb2
JVFCODE <164587661+JVFCODE@users.noreply.github.com>
2025-01-14 21:09:29 -0300

    Create app.java

---
+++ app.java
@@ -0,0 +1,13 @@
+package JVFCODE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
